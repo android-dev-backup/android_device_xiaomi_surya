6c56e3d01267b900371ca7efbba7b45377d02d99
SawaMEN <az09209@yandex.ru>
2021-01-05 13:48:00 +0700

    Fixed more Thermal errors

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -73,10 +73,6 @@ public final class ThermalUtils {
         PreferenceManager.getDefaultSharedPreferences(context).edit().putString(THERMAL_SERVICE, "false").apply();
     }
 
-    protected static boolean isServiceEnabled(Context context) {
-        return Boolean.valueOf(PreferenceManager.getDefaultSharedPreferences(context).getString(THERMAL_SERVICE, "false"));
-    }
-
     private void writeValue(String profiles) {
         mSharedPrefs.edit().putString(THERMAL_CONTROL, profiles).apply();
     }
