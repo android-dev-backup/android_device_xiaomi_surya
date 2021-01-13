075b410516265c63f3c43b89befec206e182a181
SawaMEN <az09209@yandex.ru>
2021-01-13 19:44:24 +0700

    Disable SDM rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.

--- product_prop.mk
+++ product_prop.mk
@@ -43,7 +43,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    vendor.display.disable_rotator_downscale=1
 
 # WFD
 PRODUCT_PRODUCT_PROPERTIES += \
